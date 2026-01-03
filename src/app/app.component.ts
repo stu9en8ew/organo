0cc0575b099deca8984bc7e6dcce12a587ce4ac2
Márcio Marques <mar.info.rj@gmail.com>
2026-01-03 17:32:46 +0100

    Inizio progetto organo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CabecalhoComponent,
+    RodapeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
